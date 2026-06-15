62bb7b3f25a018a578f7eb72c8c00d39813a5f58
refa <syedarefa1402@gmail.com>
2026-06-15 14:44:37 +0530

    Add Terraform configuration for AWS and Vault providers

---
+++ bootstrap/versions.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "vault" {
+  address = var.vault_addr
+
+  skip_child_token = true
+
+  auth_login {
+    path = "auth/approle/login"
+
+    parameters = {
+      role_id   = var.vault_role_id
+      secret_id = var.vault_secret_id
+    }
+  }
+}
+
+data "vault_kv_secret_v2" "aws" {
+  mount = "secret"
+  name  = "aws"
+}
+
+provider "aws" {
+  region = var.aws_region
+
+  access_key = data.vault_kv_secret_v2.aws.data["access_key"]
+  secret_key = data.vault_kv_secret_v2.aws.data["secret_key"]
+}
